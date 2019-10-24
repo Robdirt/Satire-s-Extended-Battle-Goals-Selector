bc9c460c289da173434136ccd476ba80cc7684fb
Robdirt <rob74864@yahoo.com>
2019-10-23 21:12:16 -0500

    Allow the user to select how many goals to draw and add some error checking

--- NumberGenerator/Generator.cs
+++ NumberGenerator/Generator.cs
@@ -5,16 +5,18 @@ using System.Linq;
 
 namespace NumberGenerator
 {
-    class Generator
+    internal class Generator
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             const int maxSize = 79;
-            const int outputSize = 3;
+            string outputId = "draw size: ";
+            int outputSize = 3;
 
             try
             {
                 string fileName = Path.Combine(Environment.CurrentDirectory, "BattleGoals.txt");
+
                 if (args.Length > 1
                     && args[0].Equals("clear", StringComparison.CurrentCultureIgnoreCase)
                     && File.Exists(fileName))
@@ -22,8 +24,39 @@ namespace NumberGenerator
                     File.Delete(fileName);
                 }
 
-                List<int> inputSet = File.Exists(fileName)
-                                    ? File.ReadAllText(fileName).Split('\n').Select(num => int.Parse(num)).ToList()
+                List<string> lines = new List<string>();
+                if (File.Exists(fileName))
+                {
+                    lines.AddRange(File.ReadAllText(fileName).Split('\n'));
+                }
+
+                if (lines.FirstOrDefault()?.Contains(outputId) ?? false)
+                {
+                    outputSize = int.Parse(lines.First().Replace(outputId, string.Empty));
+                }
+                else
+                {
+                    bool isValid = false;
+                    do
+                    {
+                        Console.Write("How many battle goals do you want to draw? ");
+                        string value = Console.ReadLine();
+                        if (value.All(char.IsDigit))
+                        {
+                            int number = int.Parse(value);
+                            if (number > 0 && number < maxSize / 2)
+                            {
+                                isValid = true;
+                                outputSize = number;
+                            }
+                        }
+                        if (!isValid)
+                            Console.WriteLine("Invalid Input try again.\n");
+                    } while (!isValid);
+                }
+
+                List<int> inputSet = lines.Any()
+                                    ? lines.Where(line => line.All(char.IsDigit)).Select(num => int.Parse(num)).ToList()
                                     : new List<int>();
 
                 Console.WriteLine($"{inputSet.Count} Battle Goals have previously been drawn.");
@@ -45,7 +78,7 @@ namespace NumberGenerator
                     Console.WriteLine($"Battle goal options:  {string.Join(", ", myPoints)}");
                     Console.WriteLine();
 
-                    File.WriteAllText(Path.Combine(fileName, fileName), string.Join("\n", inputSet.Concat(myPoints)));
+                    File.WriteAllText(Path.Combine(fileName, fileName), $"{outputId}{outputSize}\n{string.Join("\n", inputSet.Concat(myPoints))}");
 
                     Console.WriteLine("Drawn cards will not be drawn again.");
                 }
@@ -54,6 +87,7 @@ namespace NumberGenerator
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Not Enough cards in the deck to draw {outputSize} cards. Log File will be deleted.");
                     File.Delete(fileName);
+                    File.WriteAllText(Path.Combine(fileName, fileName), $"{outputId}{outputSize}");
                 }
             }
             catch (Exception e)
